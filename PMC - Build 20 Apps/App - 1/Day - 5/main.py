336bf6cc371f12889a7653e0f056bded894149e4
Mrutyunjay Patel <officialmrutyunjay@gmail.com>
2024-07-15 01:50:12 +0530

    Additional code

--- PMC - Build 20 Apps/App - 1/Day - 5/main.py
+++ PMC - Build 20 Apps/App - 1/Day - 5/main.py
@@ -1,29 +1,43 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: \n")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: \n")
             todos.append(todo)
         case "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print("\n")
+                print(f"{index+1} - {item}\n")
         case "edit":
-            print("Enter the todo number for which you want to edit from below: ")
-            for item in todos:
-                print(item)
+            print("Enter the todo number for which you want to edit from below: \n")
+            for index, item in enumerate(todos):
+                print("\n")
+                print(f"{index+1} - {item}\n")
             number = int(input())
             number = number - 1
-            print(f"Enter the todo that you want to replace with - \"{todos[number]}\"")
+            print(f"Enter the todo that you want to replace with - \"{todos[number]}\"\n")
             new_todo = input()
             todos[number] = new_todo.strip()
             print("Removed Successfully !! Your New Todos are: ")
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print("\n")
+                print(f"{index+1} - {item}\n")
+        case "complete":
+            print("Enter the todo number for which you want to complete the task from below: \n")
+            for index, item in enumerate(todos):
+                print("\n")
+                print(f"{index+1} - {item}\n")
+            com_num = int(input())
+            todos.pop(com_num - 1)
+            print("Removed Successfully !! Your New Todos are: \n")
+            for index, item in enumerate(todos):
+                print("\n")
+                print(f"{index+1} - {item}\n")
         case "exit":
             break
 
-print("Bye!!")
+print("\n Bye!!")
